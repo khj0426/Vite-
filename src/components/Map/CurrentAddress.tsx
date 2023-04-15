8518485515e0c7572c5ec3834629ab211c48ba1d
Kim0426 <706shin1728@naver.com>
2023-04-15 14:48:30 +0900

    fix:Detail 라우터 경로 수정

--- src/components/Map/CurrentAddress.tsx
+++ src/components/Map/CurrentAddress.tsx
@@ -23,7 +23,7 @@ function CurrentAddress({ resultAddress }: AddressHandlerType) {
         <StyledCurrentAddress>
           <StyledTitle>{`${resultAddress[0]?.address?.address_name}근처의 모든 것을 만나고 싶다면?`}</StyledTitle>
 
-          <Link to="/Detail">
+          <Link to="/Vite-React/Detail">
             <Button style={{ fontWeight: 700, width: '170px' }}>
               지금 만나러 가기{' '}
             </Button>
