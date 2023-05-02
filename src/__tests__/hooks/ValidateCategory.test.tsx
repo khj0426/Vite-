290ce3d6bf78b5b126a5db093bfc598e423ccedf
Kim0426 <706shin1728@naver.com>
2023-05-02 14:16:22 +0900

    test:isValidCategory test
    올바른 카테고리인지 아닌지 확인하는 테스트코드 작성

---
+++ src/__tests__/hooks/ValidateCategory.test.tsx
@@ -0,0 +1,22 @@
+import isValidCategory from '../../hook/Domain/Search/isValidCategory';
+import { describe, test, expect } from 'vitest';
+
+describe('Validate Category', () => {
+  test('should return false for invalid input', () => {
+    expect(isValidCategory('')).toBe(false);
+    expect(isValidCategory(undefined)).toBe(false);
+    expect(isValidCategory(null)).toBe(false);
+    expect(isValidCategory({})).toBe(false);
+    expect(isValidCategory([])).toBe(false);
+    expect(isValidCategory(123)).toBe(false);
+  });
+
+  test('should return category code for valid input', () => {
+    expect(isValidCategory('AD5')).toBe('AD5');
+    expect(isValidCategory('AC5')).toBe('AC5');
+    expect(isValidCategory('AG2')).toBe('AG2');
+    expect(isValidCategory('BK9')).toBe('BK9');
+    expect(isValidCategory('CS2')).toBe('CS2');
+    expect(isValidCategory('FD6')).toBe('FD6');
+  });
+});
